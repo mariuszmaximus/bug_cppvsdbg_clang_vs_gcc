604751f5efbf3a41c37bae6a2d31325bb99c22a5
mariuszmaximus <mariusz.maximus@gmail.com>
2023-12-23 17:49:33 +0100

    sample natvis

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 int main() {
     std::vector<int> bar{1, 2, 3, 4, 5, 6};
+    bar.reserve(10);
     std::string foo = "Hello ";
     foo += "World";
     std::cout << foo << std::endl;
---
+++ toolset.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_C_COMPILER "C:/msys64/clang64/bin/clang.exe")
+set(CMAKE_CXX_COMPILER "C:/msys64/clang64/bin/clang++.exe")
+
+# set(CMAKE_C_COMPILER "C:/msys64/mingw64/bin/gcc.exe")
+# set(CMAKE_CXX_COMPILER "C:/msys64/mingw64/bin/g++.exe")
+ 
